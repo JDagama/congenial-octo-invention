752af404be1adf3f9b0f9b676dc518195cd2f7df
José Agama <jagama_mo@outlook.es>
2023-11-19 16:18:09 -0500

    add instructions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [imagePrompt, setImagePrompt] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Logic for image analysis goes here
+  };
+
+  const handleImageGeneration = () => {
+    // Logic for image generation goes here
+  };
+
+  return (
+    <div>
+      <h1>Image Analysis and Generation</h1>
+      <input type="text" placeholder="Enter image URL" value={imageUrl} onChange={e => setImageUrl(e.target.value)} />
+      <input type="text" placeholder="Enter image prompt" value={imagePrompt} onChange={e => setImagePrompt(e.target.value)} />
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
